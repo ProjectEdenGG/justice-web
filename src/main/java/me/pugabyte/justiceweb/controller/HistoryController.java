297bcaf7ac4a5c7bd0ba5a3c5f2339c1e4a137f4
Pugabyte <pugabyte@projecteden.gg>
2021-04-21 23:20:56 -0500

    Fix template

--- src/main/java/me/pugabyte/justiceweb/controller/HistoryController.java
+++ src/main/java/me/pugabyte/justiceweb/controller/HistoryController.java
@@ -1,18 +1,11 @@
 package me.pugabyte.justiceweb.controller;
 
-import edenapi.models.nerd.Nerd;
-import edenapi.models.nerd.NerdService;
 import edenapi.models.punishments.PunishmentsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.UUID;
-
-import static me.pugabyte.justiceweb.view.View.HISTORY_INDEX;
 
 @Controller
 @RequestMapping("history")
@@ -21,8 +14,8 @@ public class HistoryController {
 	@GetMapping("/{player}")
 	public String history(@PathVariable("player") String player, Model model) {
 		model.addAttribute("name", player);
-		model.addAttribute("history", new PunishmentsService().get(player).toPrettyString());
-		return HISTORY_INDEX.view();
+		model.addAttribute("history", new PunishmentsService().get(player));
+		return "history/index";
 	}
 
 }
