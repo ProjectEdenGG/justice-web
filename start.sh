b04865b8a362676184c374c933a87ec27a945e7c
GriffinCodes <griffin@projecteden.gg>
2025-09-22 12:38:28 -0500

    Update versions, add start script

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-java -jar --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED justice-web.jar
+#!/bin/bash
+java -jar \
+--add-opens java.base/java.util=ALL-UNNAMED \
+--add-opens java.base/java.lang=ALL-UNNAMED \
+justice-web.jar
